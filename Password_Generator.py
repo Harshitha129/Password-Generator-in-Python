7aa3f7113cee719d1a69758df6c1e0389132809d
Harshitha <89914609+Harshitha129@users.noreply.github.com>
2023-12-01 18:40:53 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,3 +1,9 @@
+''' 
+This is Password Genarator
+With this you can genarate a random password
+'''
+
+
 import random
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
@@ -21,3 +27,6 @@ password=""
 for char in password_list:
 	password += char
 print(password)
+
+'''  '''
+
